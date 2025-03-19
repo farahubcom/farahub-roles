9d64d29cfff8aa3c6c84495f51e7859f0b0ff319
Farzin Kakh <farzinask1996@gmail.com>
2025-03-19 18:35:11 +0330

    people list and createOrUpdate hooks improvement

--- src/hooks.js
+++ src/hooks.js
@@ -1,7 +1,8 @@
 
 const map = require('lodash/map');
 const uniqBy = require('lodash/uniqBy');
-const { Lang } = require('@farahub/framework/facades');
+const { Lang, Doc, Injection } = require('@farahub/framework/facades');
+const toLower = require('lodash/toLower');
 
 
 const hooks = module => ({
@@ -39,7 +40,16 @@ const hooks = module => ({
     'People': {
         'main.list.search': async ({ req, user }) => {
             const args = req.query;
-            return { roles: args.roles }
+
+            if (args && args.roles) {
+                const Role = req.wsConnection.model('Role');
+                const role = typeof args.roles == "string" ?
+                    await Doc.resolveByIdentifier(args.roles, Role) :
+                    await Doc.resolve(args.roles, Role);
+                return role?.id ? { roles: role?.id } : { _id: null };
+            }
+
+            return {}
         },
         'main.list.populate': async () => {
             return {
@@ -47,44 +57,83 @@ const hooks = module => ({
                 select: 'name'
             }
         },
-        // 'main.createOrUpdate.validate': async () => {
-        //     return {
-        //         roles: {
-        //             in: ["body"],
-        //             optional: true,
-        //             isArray: true,
-        //         },
-        //         'roles.*': {
-        //             in: ["body"],
-        //             custom: {
-        //                 options: (value, { req }) => {
-        //                     const Role = req.wsConnection.model('Role');
-        //                     return Doc.resolve(value, Role).then(role => {
-        //                         if (!role)
-        //                             return Promise.reject(false);
-        //                         return Promise.resolve(true);
-        //                     })
-        //                 },
-        //                 bail: true
-        //             },
-        //             customSanitizer: {
-        //                 options: (value, { req }) => {
-        //                     const Role = req.wsConnection.model('Role');
-        //                     return Doc.resolve(value, Role);
-        //                 }
-        //             }
-        //         }
-        //     }
-        // },
+        'main.createOrUpdate.validate': async () => {
+            return {
+                roles: {
+                    in: ["body"],
+                    optional: true,
+                    isArray: true,
+                },
+                'roles.*': {
+                    in: ["body"],
+                    custom: {
+                        options: (value, { req }) => {
+                            const Role = req.wsConnection.model('Role');
+
+                            if (typeof value === 'string') {
+                                return Doc.resolveByIdentifier(value, Role).then(role => {
+                                    if (!role) {
+                                        if (!Object.keys(module.app.roles).includes(value)) {
+                                            return Promise.reject('نقش نامعتبر است');
+                                        }
+                                    }
+
+                                    return Promise.resolve(true);
+                                })
+                            }
+
+                            return Doc.resolve(value, Role).then(role => {
+                                if (!role) {
+                                    return Promise.reject(false);
+                                }
+
+                                return Promise.resolve(true);
+                            })
+                        },
+                        bail: true
+                    },
+                }
+            }
+        },
         'main.details.populate': async () => {
             return {
                 path: 'roles',
                 select: 'name'
             }
         },
-        // 'main.createOrUpdate.preSave': async ({ data, connection, inject, person }) => {
-        //     person.roles = data.roles?.map(role => role.id);
-        // },
+        'main.createOrUpdate.preSave': async ({ req, data, connection, inject, person }) => {
+            const Role = req.wsConnection.model('Role');
+
+            person.roles = (await Promise.all(
+                data.roles?.filter(Boolean)?.map(
+                    async role => {
+                        if (typeof role === "string") {
+                            const roleDoc = await Doc.resolveByIdentifier(role, Role);
+
+                            if (roleDoc) {
+                                role = roleDoc;
+                            } else if (Object.keys(module.app.roles).includes(role)) {
+                                role = await Role.createOrUpdate(
+                                    {
+                                        identifier: toLower(role),
+                                        ...module.app.roles[role],
+                                    },
+                                    null,
+                                    {
+                                        connection,
+                                        inject: Injection.register(module.app.module('Roles'), 'main.createOrUpdate', { withRequest: false })
+                                    }
+                                )
+                            }
+                        } else {
+                            role = await Doc.resolve(role, Role);
+                        }
+
+                        return role?.id;
+                    }
+                )
+            )).filter(Boolean);
+        },
     }
 })
 
